e9be04d76d2cb1ca435ff6ec8cb827d2f98c27f5
hopde111 <mamurovesentur@gmail.com>
2024-12-07 19:17:36 +0600

    адилет: добавил файл module.py; изменил файл main.py

---
+++ module.py
@@ -0,0 +1,15 @@
+STUDENTS_LIST = [
+    "Esen M.",
+    "Altynai A.",
+    "Azamat T.",
+    "Kylych S.",
+]
+
+MAX_SCORE = 30
+
+STUDENTS_GRADES = (
+    (15, MAX_SCORE),
+    (12, MAX_SCORE),
+    (23, MAX_SCORE),
+    (12, MAX_SCORE),
+)
